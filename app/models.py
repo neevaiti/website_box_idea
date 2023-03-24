f18573dc69bb8fcf0effcb2f97ab08b95ca102b3
Ne'evaiti <52328785+neevaiti@users.noreply.github.com>
2023-03-24 06:46:32 +0100

    🚧- Modify with Docker hosting.

--- app/models.py
+++ app/models.py
@@ -3,26 +3,42 @@ from django.contrib.auth.models import Group, Permission
 from django.db import models
 from django.contrib.auth.models import User
 
-class User(User):
-    # champs supplémentaires pour stocker l'image de profil
-    image = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
-    # autres champs de l'utilisateur
-    date_of_birth = models.DateField(blank=True, null=True)
-    bio = models.TextField(max_length=500, blank=True)
 
-    def __str__(self):
-        return self.username
-
-
-
-class PostIdea(models.Model):
-    """
-    This class represents the model of the idea
-    """
-    title = models.CharField(max_length=200)
+class Idea(models.Model):
+    title = models.CharField(max_length=100)
     description = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ideas')
+    likes = models.ManyToManyField(User, related_name='ideas_liked', blank=True)
+    dislikes = models.ManyToManyField(User, related_name='ideas_disliked', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+    @property
+    def likes_count(self):
+        return self.likes.count()
+
+    @property
+    def dislikes_count(self):
+        return self.dislikes.count()
+
+
+class Comment(models.Model):
+    idea = models.ForeignKey(Idea, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    likes = models.ManyToManyField(User, related_name='comments_liked', blank=True)
+    dislikes = models.ManyToManyField(User, related_name='comments_disliked', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author.username}'s comment on '{self.idea.title}'"
+
+    @property
+    def likes_count(self):
+        return self.likes.count()
+
+    @property
+    def dislikes_count(self):
+        return self.dislikes.count()
